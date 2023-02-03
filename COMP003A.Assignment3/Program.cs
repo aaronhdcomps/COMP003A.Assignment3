ac8fddaaa4f0343a2dfae8bb76b2ce95e554c9af
aaronhdcomps <aaron.hdcomps@gmail.com>
2023-02-03 13:40:39 -0800

    Finished nested if else and added comments.

--- COMP003A.Assignment3/Program.cs
+++ COMP003A.Assignment3/Program.cs
@@ -10,13 +10,49 @@ namespace COMP003A.Assignment3
     {
         static void Main(string[] args)
         {
+            //If-Else Section (Numeric to Letter Grade)-----------------------------
+
             Console.WriteLine("**************************************************");
             Console.WriteLine("If-Else Section (Numeric to Letter Grade)         ");
             Console.WriteLine("**************************************************");
+
+            //User input prompt for grade number
             Console.WriteLine("\n\n");
             Console.Write("Enter your Numeric Grade (1-100) ");
-            double numericGrade = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Your numeric grade is " + numericGrade);
+            double numericGrade = Convert.ToDouble(Console.ReadLine());//----------<<<|User input is converted to double type and stored in variable.
+            Console.WriteLine($"The numeric grade entered is {numericGrade}.");
+
+            //The nested if-else statement
+            if (numericGrade >= 0 && numericGrade <= 100)
+            {
+                if (numericGrade >= 90)
+                {
+                    Console.WriteLine("Letter Grade: A");
+                }
+                else if (numericGrade >= 80)
+                {
+                    Console.WriteLine("Letter Grade: B");
+                }
+                else if (numericGrade >= 70)
+                {
+                    Console.WriteLine("Letter Grade: C");
+                }
+                else if (numericGrade >= 60)
+                {
+                    Console.WriteLine("Letter Grade: D");
+                }
+                else
+                {
+                    Console.WriteLine("Letter Grade: F");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Input Invalid");
+            }
+            //---------------------------------------------------------------------
+
+            //
         }
     }
 }
